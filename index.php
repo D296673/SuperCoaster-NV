04481ebb54ae63de2b357218d0c5c58bf06c4b88
D296673 <121010554+D296673@users.noreply.github.com>
2023-03-24 10:07:15 +0100

    img en header center

--- index.php
+++ index.php
@@ -23,10 +23,14 @@
 </head>
 
 <body>
-
+  <div class="body">
   <!-- Add your site or application content here -->
-  <h1>supercouster-nv</h1>
-  <img src="img/achtbaan.png" alt="achtbaan">
+    <h1>supercouster-nv</h1>
+    <div class="img">
+      <img src="img/achtbaan.png" alt="achtbaan">
+    </div>
+  </div>
+
 
   <script src="js/vendor/modernizr-3.11.2.min.js"></script>
   <script src="js/plugins.js"></script>
